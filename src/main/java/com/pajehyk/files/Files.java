c09b0352af0842016fa99dffe029801c540ea859
Kirill Popov <pajehyk@gmail.com>
2021-11-01 22:35:28 +0300

    Cosmetic changes and javadoc comments

--- src/main/java/com/pajehyk/files/Files.java
+++ src/main/java/com/pajehyk/files/Files.java
@@ -3,13 +3,28 @@ package com.pajehyk.files;
 import java.io.File;
 import java.io.FileOutputStream;
 
+/**
+ * Files class contains main() method that should recieve two arguments from command line 
+ * which are chosen directory and output file in which the program writes paths of files,
+ * containing in the directory.
+ */
 public class Files {
     private static String directoryPath;
     private static String outputPath;
     private static File directoryFile;
     private static File outputPathFile;
-    public static void main(String[] args) throws Throwable {
-        if (args.length != 2) throw new InvalidArgumentsException();
+
+    /**
+     * main() method that recieves path of directory and path of output file and write down 
+     * all files that contains in the directory into output file.
+     *
+     * @param args                          command line arguments
+     * @throws InvalidArgumentsException    throws exception in case of wrong set of arguments    
+     */
+    public static void main(String[] args) throws InvalidArgumentsException {
+        if (args.length != 2) { 
+            throw new InvalidArgumentsException();
+        }
         directoryPath = args[0];
         outputPath = args[1];
         directoryFile = new File(directoryPath);
@@ -17,6 +32,13 @@ public class Files {
         printDirectoryTree(directoryFile, outputPathFile);
     }
 
+    /**
+     * recursive method that walk all over the directory tree and write down files' paths to the
+     * output file.
+     *
+     * @param directoryFile     path of the directory
+     * @param outputPathFile    path of output file
+     */
     public static void printDirectoryTree(File directoryFile, File outputPathFile) {
         File[] files = directoryFile.listFiles();
         if (files == null || files.length == 0) {
@@ -25,12 +47,11 @@ public class Files {
                 String currentDirectoryPath = directoryFile.getPath() + '\n';
                 fos.write(currentDirectoryPath.getBytes());
                 fos.close();
-            }
-            catch (Throwable exc) {
+            } catch (Throwable exc) {
                 exc.printStackTrace();
             }
         } else {
-            for (File file: files) {
+            for (File file : files) {
                 printDirectoryTree(file, outputPathFile);
             }
         }
--- src/main/java/com/pajehyk/files/InvalidArgumentsException.java
+++ src/main/java/com/pajehyk/files/InvalidArgumentsException.java
@@ -1,5 +1,8 @@
 package com.pajehyk.files;
 
+/**
+ * Exception that thrown in case of wrong number of passed arguments.
+ */
 public class InvalidArgumentsException extends Throwable {
 
 }
