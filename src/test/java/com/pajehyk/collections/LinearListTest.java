834de64e2b2a10abe88c34d1d0f277f717f0f18a
Kirill Popov <pajehyk@gmail.com>
2021-11-06 22:19:01 +0300

    Added LinearListTest class

--- src/test/java/com/pajehyk/collections/LinearListTest.java
+++ src/test/java/com/pajehyk/collections/LinearListTest.java
@@ -5,21 +5,65 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LinearListTest {
+    private Integer[] array;
+    private LinearList<Integer> list;
+    @BeforeEach
+    public void initMethod() {
+        array = new Integer[5];
+        for (int i = 0; i < 5; i++) {
+            array[i] = i;
+        }
+        list = new LinearList<Integer>(array);
+    }
     @Test
     public void addTest() {
-        Integer[] array = new Integer[5];
         Integer[] anotherArray = new Integer[6];
-        for (int i=0; i<5; i++) {
-            array[i] = i;
+        for (int i=0; i < 6; i++) {
             anotherArray[i] = i;
         }
-        anotherArray[5] = 5;
-        LinearList<Integer> list = new LinearList<Integer>(array);
         LinearList<Integer> anotherList = new LinearList<Integer>(anotherArray);
         list.add(5);
         assertEquals(list, anotherList);
+    }
+    @Test
+    public void addByIndexTest() {
+        Integer[] anotherArray = new Integer[6];
+        for (int i=0; i < 6; i++) {
+            if (i < 2) {
+                anotherArray[i] = i;
+            }
+            if (i > 2) {
+                anotherArray[i] = i-1;
+            }
+        }
+        anotherArray[2] = 10;
+        LinearList<Integer> anotherList = new LinearList<Integer>(anotherArray);
+        list.add(2, 10);
+        System.out.println(list);
+        System.out.println(anotherList);
+        assertEquals(list, anotherList);
+    }
+    @Test
+    public void indexOfTest() {
+        int index = list.indexOf(4);
+        assertEquals(4, index);
+    }
+    @Test
+    public void sizeTest() {
+        int size = list.size();
+        assertEquals(5, size);
+    }
+    @Test
+    public void equalsTest() {
+        Integer[] anotherArray = new Integer[5];
+        for (int i=0; i < 5; i++) {
+            anotherArray[i] = i;
+        }
+        LinearList<Integer> anotherList = new LinearList<Integer>(anotherArray);
+        assertTrue(list.equals(anotherList));
     }
 }
