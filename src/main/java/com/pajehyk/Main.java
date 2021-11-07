a2c6878462364e5cc60d69195a28d79d18fa0a56
Kirill Popov <pajehyk@gmail.com>
2021-11-07 13:02:58 +0300

    deleted code inherited from main branch

--- src/main/java/com/pajehyk/Main.java
+++ src/main/java/com/pajehyk/Main.java
@@ -1,15 +1,7 @@
 package com.pajehyk;
 
-import java.io.FileNotFoundException;
-
-import com.pajehyk.atm.Atm;
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            Atm.run();
-        } catch(FileNotFoundException exc) {
-            System.out.println("File not found");
-        }
+        System.out.println("Hello, world!");
     }
 }
