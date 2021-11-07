202e59e894e691fa1b02e6921a57a1a96f323dbb
Kirill Popov <pajehyk@gmail.com>
2021-11-07 18:34:06 +0300

    invoking run method from main

--- src/main/java/com/pajehyk/Main.java
+++ src/main/java/com/pajehyk/Main.java
@@ -1,7 +1,14 @@
 package com.pajehyk;
 
+import com.pajehyk.files.Files;
+import com.pajehyk.files.InvalidArgumentsException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        try {
+            Files.run(args);
+        } catch (InvalidArgumentsException exc) {
+            System.out.println("There must be two arguments.");
+        }
     }
 }
