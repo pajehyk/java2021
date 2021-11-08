48d022866d27c7d1769b61ab31aee5d9308e97b4
Kirill Popov <pajehyk@gmail.com>
2021-11-08 17:59:46 +0300

    minor changes

--- src/main/java/com/pajehyk/Main.java
+++ src/main/java/com/pajehyk/Main.java
@@ -5,14 +5,14 @@ import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.Scanner;
 
-import com.pajehyk.multithread.MyThread;
+import com.pajehyk.multithread.Compute;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         Scanner scanner = new Scanner(new File("src/main/resources/inputFile.txt"));
         PrintStream printStream = new PrintStream(new File("src/main/resources/outputFile.txt"));
-        printStream = System.out;
-        MyThread myThread1 = MyThread.createAndStartThread(scanner, printStream, "Thread 1");
-        MyThread myThread2 = MyThread.createAndStartThread(scanner, printStream, "Thread 2");
+        Compute compute = new Compute();
+        compute.ComputeWithThreads(scanner, printStream, 2);
+        System.out.println(Math.tan(0.6084669908981728));
     }
 }
