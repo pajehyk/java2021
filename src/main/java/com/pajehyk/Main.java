a11e489d5dfe0cc2c9a37c294cd4462c6ee1d8b7
Kirill Popov <pajehyk@gmail.com>
2021-11-08 15:36:22 +0300

    main class calling threads

--- src/main/java/com/pajehyk/Main.java
+++ src/main/java/com/pajehyk/Main.java
@@ -1,7 +1,15 @@
 package com.pajehyk;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import com.pajehyk.mio.MyInputStream;
+import com.pajehyk.multithread.MyThread;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+        MyInputStream fis = new MyInputStream(new File("src/main/resources/inputFile.txt"));
+        MyThread myThreadGreen = MyThread.createAndStartThread(fis, "Thread 1");
+        MyThread myThreadRed = MyThread.createAndStartThread(fis, "Thread 2");
     }
 }
