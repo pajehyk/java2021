4c0250a470412c4367155e6c14d4e8871d0e4d4c
Kirill Popov <pajehyk@gmail.com>
2021-11-22 19:46:43 +0300

    Deleted old code

--- src/main/java/com/pajehyk/Main.java
+++ src/main/java/com/pajehyk/Main.java
@@ -5,13 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.Scanner;
 
-import com.pajehyk.multithread.Compute;
-
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
-        Scanner scanner = new Scanner(new File("src/test/resources/input1000000.txt"));
-        PrintStream printStream = new PrintStream(new File("src/main/resources/outputFile.txt"));
-        Compute compute = new Compute();
-        compute.ComputeWithThreads(scanner, printStream, 2);
+
     }
 }
